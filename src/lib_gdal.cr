af907f1651774056d0d2d53e4bf0a25c1b82c64b
Corey Morello <cmorello@weatherflow.com>
2024-05-23 14:04:09 -0700

    Organized everything a bit

--- src/lib_gdal.cr
+++ src/lib_gdal.cr
@@ -1,20 +1,19 @@
 @[Link(ldflags: "`gdal-config --libs`")]
 lib LibGDAL
-  fun all_register = GDALAllRegister : Void
-  alias GDALDriverH = Void*
-  fun get_driver_by_name = GDALGetDriverByName(name : UInt8*) : GDALDriverH
-
+  alias Err = Int32
   alias CSLConstList = Char**
-  fun create = GDALCreate(driver : GDALDriverH, filename : UInt8*, width : LibC::Int, height : LibC::Int, bands : LibC::Int, type : DataType, options : CSLConstList) : GDALDatasetH
+  alias GDALDriverH = Void*
+  alias GDALDatasetH = Void*
+  alias GDALRasterBandH = Void*
+  alias GDALColorTableH = Void*
+  alias GDALWarpOptions = Char**
   alias GDALProgressFunc = Pointer((Int32, Float64, LibC::Char*, Void*) -> Void)
-  fun create_copy = GDALCreateCopy(driver : GDALDriverH, filename : UInt8*, dataset : GDALDatasetH, size : LibC::Int, options : CSLConstList, progress_func : GDALProgressFunc, pProgressData : Void*) : GDALDatasetH
+  alias VSILFILE = Void*
+
   enum Access
     ReadOnly = 0
     Update   = 1
   end
-
-  alias GDALDatasetH = Void*
-  fun open = GDALOpen(filename : Char*, access : Access) : GDALDatasetH
 
   enum CPLErr
     CE_None    = 0
@@ -24,15 +23,6 @@ lib LibGDAL
     CE_Fatal   = 4
   end
 
-  fun close = GDALClose(dataset : GDALDatasetH) : CPLErr
-
-  fun get_raster_count = GDALGetRasterCount(dataset : GDALDatasetH) : LibC::Int
-
-  alias GDALRasterBandH = Void*
-  fun get_raster_band = GDALGetRasterBand(dataset : GDALDatasetH, band : LibC::Int) : GDALRasterBandH
-  fun get_raster_x_size = GDALGetRasterXSize(dataset : GDALDatasetH) : LibC::Int
-  fun get_raster_y_size = GDALGetRasterYSize(dataset : GDALDatasetH) : LibC::Int
-  fun get_no_data_value = GDALGetRasterNoDataValue(raster_band : GDALRasterBandH, err : LibC::Int*) : LibC::Double
   enum DataType
     GDT_Unknown
     GDT_Byte
@@ -51,33 +41,11 @@ lib LibGDAL
     GDT_CFloat64
     GDT_TypeCount
   end
-  fun get_raster_data_type = GDALGetRasterDataType(dataset : GDALDatasetH) : DataType
 
-  alias GDALColorTableH = Void*
-  fun get_raster_color_table = GDALGetRasterColorTable(raster_band : GDALRasterBandH) : GDALColorTableH
-
-  fun get_projection_ref = GDALGetProjectionRef(dataset : GDALDatasetH) : Char*
   enum RWFlag
     Read  = 0
     Write = 1
   end
-  alias Err = Int32
-  fun raster_io = GDALRasterIO(
-    hRBand : GDALRasterBandH,
-    eRWFlag : RWFlag,
-    nDSXOff : Int32,
-    nDSYOff : Int32,
-    nDSXSize : Int32,
-    nDSYSize : Int32,
-    pBuffer : Void*,
-    nBXSize : Int32,
-    nBYSize : Int32,
-    eBDataType : DataType,
-    nPixelSpace : Int32,
-    nLineSpace : Int32
-  ) : Err
-
-  fun get_geo_transform = GDALGetGeoTransform(dataset : GDALDatasetH, padfGeoTransform : Pointer(Float64)) : CPLErr
 
   enum GDALResampleAlg
     NearestNeighbor
@@ -94,20 +62,54 @@ lib LibGDAL
     Q3
   end
 
-  alias GDALWarpOptions = Char**
-  fun reproject_image = GDALReprojectImage(src_dataset : GDALDatasetH, psz_src_wkt : Char*, dst_dataset : GDALDatasetH, psz_dst_wkt : Char*, resample_alg : GDALResampleAlg, warp_mem_limit : LibC::Double, max_error : LibC::Double, progress_func : GDALProgressFunc, progressArg : Void*, options : GDALWarpOptions) : CPLErr
-
-  alias VSILFILE = Void*
-  fun vsif_open = VSIFOpenL(file_name : UInt8*, access : UInt8*) : VSILFILE
   enum VSIFSeek
     Set
     Cur
     End
   end
 
+  fun all_register = GDALAllRegister : Void
+
+  fun create = GDALCreate(driver : GDALDriverH, filename : UInt8*, width : LibC::Int, height : LibC::Int, bands : LibC::Int, type : DataType, options : CSLConstList) : GDALDatasetH
+  fun create_copy = GDALCreateCopy(driver : GDALDriverH, filename : UInt8*, dataset : GDALDatasetH, size : LibC::Int, options : CSLConstList, progress_func : GDALProgressFunc, pProgressData : Void*) : GDALDatasetH
+
+  fun open = GDALOpen(filename : Char*, access : Access) : GDALDatasetH
+  fun close = GDALClose(dataset : GDALDatasetH) : CPLErr
+
+  fun get_driver_by_name = GDALGetDriverByName(name : UInt8*) : GDALDriverH
+  fun get_geo_transform = GDALGetGeoTransform(dataset : GDALDatasetH, padfGeoTransform : Pointer(Float64)) : CPLErr
+  fun get_raster_count = GDALGetRasterCount(dataset : GDALDatasetH) : LibC::Int
+  fun get_raster_band = GDALGetRasterBand(dataset : GDALDatasetH, band : LibC::Int) : GDALRasterBandH
+  fun get_raster_x_size = GDALGetRasterXSize(dataset : GDALDatasetH) : LibC::Int
+  fun get_raster_y_size = GDALGetRasterYSize(dataset : GDALDatasetH) : LibC::Int
+  fun get_no_data_value = GDALGetRasterNoDataValue(raster_band : GDALRasterBandH, err : LibC::Int*) : LibC::Double
+  fun get_raster_data_type = GDALGetRasterDataType(dataset : GDALDatasetH) : DataType
+  fun get_raster_color_table = GDALGetRasterColorTable(raster_band : GDALRasterBandH) : GDALColorTableH
+  fun get_projection_ref = GDALGetProjectionRef(dataset : GDALDatasetH) : Char*
+
+  fun raster_io = GDALRasterIO(
+    hRBand : GDALRasterBandH,
+    eRWFlag : RWFlag,
+    nDSXOff : Int32,
+    nDSYOff : Int32,
+    nDSXSize : Int32,
+    nDSYSize : Int32,
+    pBuffer : Void*,
+    nBXSize : Int32,
+    nBYSize : Int32,
+    eBDataType : DataType,
+    nPixelSpace : Int32,
+    nLineSpace : Int32
+  ) : Err
+
+
+  fun reproject_image = GDALReprojectImage(src_dataset : GDALDatasetH, psz_src_wkt : Char*, dst_dataset : GDALDatasetH, psz_dst_wkt : Char*, resample_alg : GDALResampleAlg, warp_mem_limit : LibC::Double, max_error : LibC::Double, progress_func : GDALProgressFunc, progressArg : Void*, options : GDALWarpOptions) : CPLErr
+
+  fun vsif_open = VSIFOpenL(file_name : UInt8*, access : UInt8*) : VSILFILE
+
   fun vsif_seek = VSIFSeekL(file : VSILFILE, offset : UInt8, whence : VSIFSeek) : LibC::Int
   fun vsif_tell = VSIFTellL(file : VSILFILE) : LibC::UInt64T
   fun vsif_read = VSIFReadL(buffer : Void*, size : LibC::SizeT, count : LibC::SizeT, file : VSILFILE) : LibC::SizeT
   fun vsif_close = VSIFCloseL(file : VSILFILE) : LibC::Int
   fun vsif_unlink = VSIUnlink(file_name : UInt8*) : LibC::Int
-  end
+end
